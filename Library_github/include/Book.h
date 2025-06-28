d7f23cb1d1df0a1d55df804a368c555cc1b57cb7
Duoyinan <student28wang@outlook.com>
2025-06-28 15:23:12 +0800

    Update Book.h

--- Library_github/include/Book.h
+++ Library_github/include/Book.h
@@ -1,18 +1,31 @@
 #pragma once
 #include <string>
+#include <iostream>
 
-class Book {
+enum class Role { User, Admin };
+
+class User {
 public:
-    std::string id;
-    std::string title;
-    std::string author;
-    std::string publisher;
-    int totalCount;
-    int availableCount;
+    std::string userId;
+    std::string username;
+    std::string password;
+    Role role;
 
-    Book();
-    Book(const std::string& id, const std::string& title, const std::string& author,
-         const std::string& publisher, int totalCount);
+    User() = default;
+    User(const std::string& id, const std::string& name, const std::string& pwd, Role r)
+        : userId(id), username(name), password(pwd), role(r) {}
 
-    void display() const;
+    std::string toString() const {
+        return userId + "," + username + "," + password + "," + std::to_string(static_cast<int>(role));
+    }
+
+    static User fromString(const std::string& s) {
+        User u;
+        size_t pos1 = s.find(','), pos2 = s.find(',', pos1 + 1), pos3 = s.find(',', pos2 + 1);
+        u.userId = s.substr(0, pos1);
+        u.username = s.substr(pos1 + 1, pos2 - pos1 - 1);
+        u.password = s.substr(pos2 + 1, pos3 - pos2 - 1);
+        u.role = (s.substr(pos3 + 1) == "1") ? Role::Admin : Role::User;
+        return u;
+    }
 };
