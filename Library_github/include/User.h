38cbc8006f8e0e915e62db4e4292ad87f62b344c
Duoyinan <student28wang@outlook.com>
2025-06-28 15:23:52 +0800

    Update User.h

--- Library_github/include/User.h
+++ Library_github/include/User.h
@@ -1,16 +1,31 @@
 #pragma once
 #include <string>
+#include <iostream>
+
+enum class Role { User, Admin };
 
 class User {
 public:
     std::string userId;
     std::string username;
     std::string password;
-    int role; // 0=普通用户，1=管理员
+    Role role;
 
-    User();
-    User(const std::string& id, const std::string& name, const std::string& pwd, int role);
+    User() = default;
+    User(const std::string& id, const std::string& name, const std::string& pwd, Role r)
+        : userId(id), username(name), password(pwd), role(r) {}
 
-    bool login(const std::string& username, const std::string& password);
-    void display() const;
+    std::string toString() const {
+        return userId + "," + username + "," + password + "," + std::to_string(static_cast<int>(role));
+    }
+
+    static User fromString(const std::string& s) {
+        User u;
+        size_t pos1 = s.find(','), pos2 = s.find(',', pos1 + 1), pos3 = s.find(',', pos2 + 1);
+        u.userId = s.substr(0, pos1);
+        u.username = s.substr(pos1 + 1, pos2 - pos1 - 1);
+        u.password = s.substr(pos2 + 1, pos3 - pos2 - 1);
+        u.role = (s.substr(pos3 + 1) == "1") ? Role::Admin : Role::User;
+        return u;
+    }
 };
