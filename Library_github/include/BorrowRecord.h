1ea7b653b22df61f58bf92f76808a8b4f106e106
Duoyinan <student28wang@outlook.com>
2025-06-28 15:23:32 +0800

    Update BorrowRecord.h

--- Library_github/include/BorrowRecord.h
+++ Library_github/include/BorrowRecord.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <ctime>
+#include <iostream>
 
 class BorrowRecord {
 public:
@@ -8,11 +10,23 @@ public:
     std::string bookId;
     std::string borrowDate;
     std::string returnDate;
-    bool isReturned;
 
-    BorrowRecord();
-    BorrowRecord(const std::string& recordId, const std::string& userId, const std::string& bookId,
-                 const std::string& borrowDate, const std::string& returnDate, bool isReturned);
+    BorrowRecord() = default;
+    BorrowRecord(const std::string& rid, const std::string& uid, const std::string& bid, const std::string& bdate, const std::string& rdate)
+        : recordId(rid), userId(uid), bookId(bid), borrowDate(bdate), returnDate(rdate) {}
 
-    void display() const;
+    std::string toString() const {
+        return recordId + "," + userId + "," + bookId + "," + borrowDate + "," + returnDate;
+    }
+
+    static BorrowRecord fromString(const std::string& s) {
+        BorrowRecord br;
+        size_t p1 = s.find(','), p2 = s.find(',', p1 + 1), p3 = s.find(',', p2 + 1), p4 = s.find(',', p3 + 1);
+        br.recordId = s.substr(0, p1);
+        br.userId = s.substr(p1 + 1, p2 - p1 - 1);
+        br.bookId = s.substr(p2 + 1, p3 - p2 - 1);
+        br.borrowDate = s.substr(p3 + 1, p4 - p3 - 1);
+        br.returnDate = s.substr(p4 + 1);
+        return br;
+    }
 };
